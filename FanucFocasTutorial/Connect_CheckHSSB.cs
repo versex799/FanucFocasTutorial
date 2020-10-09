2b5df087559e80b68f4b00929aa4189157a89950
Matthew Hier <mhier@cronsrud.com>
2020-10-09 11:01:08 -0400

    Added Machine positions

--- FanucFocasTutorial/Connect_CheckHSSB.cs
+++ FanucFocasTutorial/Connect_CheckHSSB.cs
@@ -1,47 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.Win32;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace FanucFocasTutorial
-{
-    class Connect_CheckHSSB
-    {
-        private static ushort _handle = 0;
-        private static short _ret = 0;
+//namespace FanucFocasTutorial
+//{
+//    class Connect_CheckHSSB
+//    {
+//        private static ushort _handle = 0;
+//        private static short _ret = 0;
 
-        public static void Connect(string[] args)
-        {
-            // If we didn't provide an IP address, use HSSB, otherwise use Ethernet
-            if (CNCHardware.HasHSSB())
-            {
-                _ret = Focas1.cnc_allclibhndl(out _handle);
-            }
-            else
-            {
-                string ipaddr = "";
+//        public static void Connect(string[] args)
+//        {
+//            // If we didn't provide an IP address, use HSSB, otherwise use Ethernet
+//            if (CNCHardware.HasHSSB())
+//            {
+//                _ret = Focas1.cnc_allclibhndl(out _handle);
+//            }
+//            else
+//            {
+//                string ipaddr = "";
 
-                // If we specified an ip address, get it from the args
-                if (args.Length > 0)
-                    ipaddr = args[0];
+//                // If we specified an ip address, get it from the args
+//                if (args.Length > 0)
+//                    ipaddr = args[0];
 
-                _ret = Focas1.cnc_allclibhndl3(ipaddr, 8193, 6, out _handle);
-            }
+//                _ret = Focas1.cnc_allclibhndl3(ipaddr, 8193, 6, out _handle);
+//            }
 
-            // Write the result to the console
-            if (_ret == Focas1.EW_OK)
-            {
-                Console.WriteLine("We are connected!");
-            }
-            else
-            {
-                Console.WriteLine("There was an error connecting. Return value: " + _ret);
-            }
+//            // Write the result to the console
+//            if (_ret == Focas1.EW_OK)
+//            {
+//                Console.WriteLine("We are connected!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("There was an error connecting. Return value: " + _ret);
+//            }
 
-            // Free the Focas handle
-            Focas1.cnc_freelibhndl(_handle);
-        }
-    }
-}
+//            // Free the Focas handle
+//            Focas1.cnc_freelibhndl(_handle);
+//        }
+//    }
+
+//    private const string baseReg =
+//    @"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
+
+//    public static bool SetMAC(string nicid, string newmac)
+//    {
+//        bool ret = false;
+//        using (RegistryKey bkey = GetBaseKey())
+//        using (RegistryKey key = bkey.OpenSubKey(baseReg + nicid))
+//        {
+//            if (key != null)
+//            {
+//                key.SetValue("NetworkAddress", newmac, RegistryValueKind.String);
+
+//                ManagementObjectSearcher mos = new ManagementObjectSearcher(
+//                    new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE Index = " + nicid));
+
+//                foreach (ManagementObject o in mos.Get().OfType<ManagementObject>())
+//                {
+//                    o.InvokeMethod("Disable", null);
+//                    o.InvokeMethod("Enable", null);
+//                    ret = true;
+//                }
+//            }
+//        }
+
+//        return ret;
+//    }
+
+//    public static IEnumerable<string> GetNicIds()
+//    {
+//        using (RegistryKey bkey = GetBaseKey())
+//        using (RegistryKey key = bkey.OpenSubKey(baseReg))
+//        {
+//            if (key != null)
+//            {
+//                foreach (string name in key.GetSubKeyNames().Where(n => n != "Properties"))
+//                {
+//                    using (RegistryKey sub = key.OpenSubKey(name))
+//                    {
+//                        if (sub != null)
+//                        {
+//                            object busType = sub.GetValue("BusType");
+//                            string busStr = busType != null ? busType.ToString() : string.Empty;
+//                            if (busStr != string.Empty)
+//                            {
+//                                yield return name;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    public static RegistryKey GetBaseKey()
+//    {
+//        return RegistryKey.OpenBaseKey(
+//            RegistryHive.LocalMachine,
+//            InternalCheckIsWow64() ? RegistryView.Registry64 : RegistryView.Registry32);
+//    }
+//}
+
+
---
+++ FanucFocasTutorial/Fanuc.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanucFocasTutorial
+{
+    public class Fanuc
+    {
+        static ushort _handle = 0;  // Handle to communicate with Fanuc
+        static short _ret = 0;  // Stores our return value
+
+
+        private double _scale = 10000;
+
+        public string GetMode()
+        {
+            // Check we have a valid handle
+            if (_handle == 0)
+                return "UNAVAILABLE";
+
+            // Creat an instance of our stucture
+            Focas1.ODBST mode = new Focas1.ODBST();
+
+            // Ask Fanuc for the status information
+            _ret = Focas1.cnc_statinfo(_handle, mode);
+
+            // Check to make sure the call was successfull
+            // and convert the mode to a string and return it.
+            if (_ret == Focas1.EW_OK)
+                return GetModeString(mode.aut);
+            return "UNAVAILABLE";
+        }
+
+        public string GetStatus()
+        {
+            if (_handle == 0)
+                return "UNAVAILABLE";
+
+            Focas1.ODBST status = new Focas1.ODBST();
+
+            _ret = Focas1.cnc_statinfo(_handle, status);
+
+            if (_ret == Focas1.EW_OK)
+                return GetModeString(status.aut);
+            return "UNAVAILABLE";
+        }
+
+        public double AbsolutePosition()
+        {
+            if (_handle == 0)
+                return 0;
+
+            Focas1.ODBAXIS _axisPositionAbsolute = new Focas1.ODBAXIS();
+            _ret = Focas1.cnc_absolute2(_handle, 88, 8, _axisPositionAbsolute);
+
+            if (_ret != Focas1.EW_OK)
+                return _ret;
+
+            return _axisPositionAbsolute.data[0] / _scale;
+        }
+
+        public double RelativePosition()
+        {
+            if (_handle == 0)
+                return 0;
+
+            Focas1.ODBAXIS _axisPositionRelative = new Focas1.ODBAXIS();
+            _ret = Focas1.cnc_relative2(_handle, 88, 8, _axisPositionRelative);
+
+            if (_ret != Focas1.EW_OK)
+                return _ret;
+
+            return _axisPositionRelative.data[0] / _scale;
+        }
+
+        public double MachinePosition()
+        {
+            if (_handle == 0)
+                return 0;
+
+            Focas1.ODBAXIS _axisPositionMachine = new Focas1.ODBAXIS();
+            _ret = Focas1.cnc_machine(_handle, 88, 8, _axisPositionMachine);
+
+            if (_ret != Focas1.EW_OK)
+                return _ret;
+
+            return _axisPositionMachine.data[0] / _scale;
+        }
+
+        // I used a switch function to demonstrate, but
+        // there are better methods out there. 
+        private string GetModeString(short mode)
+        {
+            switch (mode)
+            {
+                case 0:
+                    {
+                        return "MDI";
+                    }
+                case 1:
+                    {
+                        return "MEM";
+                    }
+                case 2:
+                    {
+                        return "****";
+                    }
+                case 3:
+                    {
+                        return "EDIT";
+                    }
+                case 4:
+                    {
+                        return "HND";
+                    }
+                case 5:
+                    {
+                        return "JOG";
+                    }
+                case 6:
+                    {
+                        return "T-JOG";
+                    }
+                case 7:
+                    {
+                        return "T-HND";
+                    }
+                case 8:
+                    {
+                        return "INC";
+                    }
+                case 9:
+                    {
+                        return "REF";
+                    }
+                case 10:
+                    {
+                        return "RMT";
+                    }
+                default:
+                    {
+                        return "UNAVAILABLE";
+                    }
+            }
+        }
+    }
+}
