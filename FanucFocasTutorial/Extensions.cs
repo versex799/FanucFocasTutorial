e9efb370ed6c2cf7791821013d6c3f3a37ad5f2f
MatthewHier <mhier@cronsrud.com>
2023-03-30 22:30:01 -0400

    Added Mode, Status, and OP Bit

---
+++ FanucFocasTutorial/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanucFocasTutorial1
+{
+    public static class Extensions
+    {
+        public static bool GetBit(this byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+
+        }
+
+        public static bool GetBit(this int b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+    }
+}
--- FanucFocasTutorial/Program.cs
+++ FanucFocasTutorial/Program.cs
@@ -1,46 +1,166 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace FanucFocasTutorial
+namespace FanucFocasTutorial1
 {
     class Program
     {
-        static ushort _handle = 0;  // Handle to communicate with Fanuc
-        static short _ret = 0;  // Stores our return value
+        static ushort _handle = 0;
+        static short _ret = 0;
+
+        static bool _exit = false;
 
         static void Main(string[] args)
         {
-            string ipaddr = "";
 
-            // If we specified an ip address, get it from the args
-            if (args.Length > 0)
-                ipaddr = args[0];
+            Thread t = new Thread(new ThreadStart(ExitCheck));
+            t.Start();
 
-            // If we didn't provide an IP address, use HSSB, otherwise use Ethernet
-            if (string.IsNullOrEmpty(ipaddr))
+            _ret = Focas1.cnc_allclibhndl3("192.168.2.123", 8193, 6, out _handle);
+
+            if (_ret != Focas1.EW_OK)
             {
-                _ret = Focas1.cnc_allclibhndl(out _handle);
+                Console.WriteLine($"Unable to connect to 192.168.2.123 on port 8193\n\nReturn Code: {_ret}\n\nExiting....");
+                Console.Read();
             }
             else
             {
-                _ret = Focas1.cnc_allclibhndl3(ipaddr, 8193, 6, out _handle);
+                Console.WriteLine($"Our Focas handle is {_handle}");
+
+                string mode = GetMode();
+                Console.WriteLine($"\n\nMode is: {mode}");
+
+                string status = GetStatus();
+                Console.WriteLine($"\n\nStatus is: {status}\n\n");
+
+                while (!_exit)
+                {
+                    Console.Write($"\rOP Signal: {GetOpSignal().ToString()}\r");
+                    Thread.Sleep(500);
+                }
+
+
+            }
+        }
+
+        private static void ExitCheck()
+        {
+            while (Console.ReadLine() != "exit")
+            {
+                continue;
             }
 
-            // Write the result to the console
-            if (_ret == Focas1.EW_OK)
+            _exit = true;
+        }
+
+
+        public static bool GetOpSignal()
+        {
+            if (_handle == 0)
             {
-                Console.WriteLine("We are connected!");
-            }
-            else
-            {
-                Console.WriteLine("There was an error connecting. Return value: " + _ret);
+                Console.WriteLine("Error: Please obtain a handle before calling this method");
+                return false;
             }
 
-            // Free the Focas handle
-            Focas1.cnc_freelibhndl(_handle);
+            short addr_kind = 1; // F
+            short data_type = 0; // Byte
+            ushort start = 0;
+            ushort end = 0;
+            ushort data_length = 9; // 8 + N
+            Focas1.IODBPMC0 pmc = new Focas1.IODBPMC0();
+
+            _ret = Focas1.pmc_rdpmcrng(_handle, addr_kind, data_type, start, end, data_length, pmc);
+
+            if (_ret != Focas1.EW_OK)
+            {
+                Console.WriteLine($"Error: Unable to ontain the OP Signal");
+                return false;
+            }
+
+            return pmc.cdata[0].GetBit(7);
+
+        }
+
+        public static string GetMode()
+        {
+            if (_handle == 0)
+            {
+                Console.WriteLine("Error: Please obtain a handle before calling this method");
+                return "";
+            }
+
+            Focas1.ODBST Mode = new Focas1.ODBST();
+
+            _ret = Focas1.cnc_statinfo(_handle, Mode);
+
+            if (_ret != 0)
+            {
+                Console.WriteLine($"Error: Unable to obtain mode.\nReturn Code: {_ret}");
+                return "";
+            }
+
+            string modestr = ModeNumberToString(Mode.aut);
+
+            return $"Mode is: {modestr}";
+        }
+
+        public static string ModeNumberToString(int num)
+        {
+            switch (num)
+            {
+                case 0: { return "MDI"; }
+                case 1: { return "MEM"; }
+                case 3: { return "EDIT"; }
+                case 4: { return "HND"; }
+                case 5: { return "JOG"; }
+                case 6: { return "Teach in JOG"; }
+                case 7: { return "Teach in HND"; }
+                case 8: { return "INC"; }
+                case 9: { return "REF"; }
+                case 10: { return "RMT"; }
+                default: { return "UNAVAILABLE"; }
+            }
+        }
+
+        public static string GetStatus()
+        {
+            if (_handle == 0)
+            {
+                Console.WriteLine("Error: Please obtain a handle before calling this method");
+                return "";
+            }
+
+            Focas1.ODBST Status = new Focas1.ODBST();
+
+            _ret = Focas1.cnc_statinfo(_handle, Status);
+
+            if (_ret != 0)
+            {
+                Console.WriteLine($"Error: Unable to obtain status.\nReturn Code: {_ret}");
+                return "";
+            }
+
+            string statusstr = StatusNumberToString(Status.run);
+
+            return $"Mode is: {statusstr}";
+        }
+
+        public static string StatusNumberToString(int num)
+        {
+            switch (num)
+            {
+                case 0: { return "****"; }
+                case 1: { return "STOP"; }
+                case 2: { return "HOLD"; }
+                case 3: { return "STRT"; }
+                case 4: { return "MSTR"; }
+                default: { return "UNAVAILABLE"; }
+            }
         }
     }
 }
